71aa697ac3ebefc0f17be23789caf9f008a65a98
erictempleton1 <erictempleton1@gmail.com>
2014-10-01 07:10:28 -0400

    updating detail template to have a link to blog post content.
    also added an import for reverse

--- project/views.py
+++ project/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView
 from project.models import Post
+from django.core.urlresolvers import reverse
 
 class PostListing(ListView):
 	model = Post
@@ -8,3 +9,4 @@ class PostListing(ListView):
 class PostCreate(CreateView):
 	model = Post
 	success_url = '/'
+
