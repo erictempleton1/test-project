0352bb9dd66d6ef580f32bee26c6518e2a5b81f2
erictempleton1 <erictempleton1@gmail.com>
2014-10-03 07:58:56 -0400

    fixed urls by adding <pk> instead.
    also tweaked template slightly

--- project/urls.py
+++ project/urls.py
@@ -3,5 +3,5 @@ from project.views import PostListing, PostCreate, PostDetail
 
 urlpatterns = patterns('', url(r'^$', PostListing.as_view(), name='listing'),
 	                       url(r'^create/$', PostCreate.as_view(), name='create'),
-	                       url(r'^(?P\d+)/$', PostDetail.as_view(), name='detail'),
+	                       url(r'^(?P<pk>\d+)/$', PostDetail.as_view(), name='detail'),
 )
