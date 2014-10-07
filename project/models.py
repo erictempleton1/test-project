23d1d22fc02576806eb83485cc52e1711f7c9b4d
Eric Templeton <erictempleton1@gmail.com>
2014-10-07 19:57:28 +0000

    spacing change

--- project/models.py
+++ project/models.py
@@ -2,15 +2,15 @@ from django.db import models
 from django.template.defaultfilters import slugify
 
 class BlogPost(models.Model):
-	title = models.CharField(max_length=100)
-	author = models.CharField(max_length=100)
-	content = models.TextField()
-	submitted = models.CharField(max_length=100)
-	slug = models.SlugField()
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    content = models.TextField()
+    submitted = models.CharField(max_length=100)
+    slug = models.SlugField()
 
-	def save(self):
-		self.slug = slugify(self.title)
-		super(BlogPost, self).save()
+    def save(self):
+        self.slug = slugify(self.title)
+        super(BlogPost, self).save()
 		
     def __unicode__(self):
         return self.title
