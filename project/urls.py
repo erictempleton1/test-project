152a339e539ff0e36e157b056be5e7620c19fc98
erictempleton1 <erictempleton1@gmail.com>
2014-10-14 07:28:40 -0400

    added blog post content page and urls
    need to add a query for author.

--- project/urls.py
+++ project/urls.py
@@ -5,5 +5,5 @@ from project.views import BlogPostCreate, HomePageView, BlogPostList
 urlpatterns = patterns('',
 	url(r'^$', HomePageView.as_view(), name='homepage'),
     url(r'create/$', login_required(BlogPostCreate.as_view()), name='create'),
-    url(r'posts/$', BlogPostList.as_view(), name='post_listing'),
+    url(r'(\d+)/(?P<slug>[\w-]+)/$', BlogPostList.as_view(), name='listing'),
 )
