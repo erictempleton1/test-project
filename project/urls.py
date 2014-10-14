b3e1a85557314919de959f0f1fbb63d079fd8cd3
erictempleton1 <erictempleton1@gmail.com>
2014-10-14 19:13:31 -0400

    added login required to update view

--- project/urls.py
+++ project/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('',
 	url(r'^$', HomePageView.as_view(), name='homepage'),
     url(r'create/$', login_required(BlogPostCreate.as_view()), name='create'),
     url(r'(\d+)/(?P<slug>[\w-]+)/$', BlogPostList.as_view(), name='listing'),
-    url(r'(\d+)/(?P<slug>[\w-]+)/update/$', BlogPostUpdate.as_view(), name='update'),
+    url(r'(\d+)/(?P<slug>[\w-]+)/update/$', login_required(BlogPostUpdate.as_view()), name='update'),
 )
