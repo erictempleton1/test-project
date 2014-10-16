ba4793468b8522234e33c0c510f770e3b3e17471
erictempleton1 <erictempleton1@gmail.com>
2014-10-16 07:11:22 -0400

    added auto save for author on blog post creation

--- project/models.py
+++ project/models.py
@@ -5,13 +5,16 @@ from django.template.defaultfilters import slugify
 class BlogPost(models.Model):
     user = models.ForeignKey(User)
     title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
     content = models.TextField()
     added = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     slug = models.SlugField()
 
     def save(self):
+    	""" Auto saves title as slug, and user as author. """
         self.slug = slugify(self.title)
+        self.author = self.user.username
         super(BlogPost, self).save()
 		
     def __unicode__(self):
