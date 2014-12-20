29753ec1470182753817a42ef2fbc9698697d4ac
erictempleton1 <erictempleton1@gmail.com>
2014-12-20 11:14:31 -0500

    testing user follow

--- project/tests.py
+++ project/tests.py
@@ -1,14 +1,29 @@
-from django.test import TestCase
+from django.test import TestCase, LiveServerTestCase
 from django.test import TestCase, Client
-from project.models import BlogPost, UserProfile
-from project.views import BlogPostCreate, HomePageView, BlogPostDetail, BlogPostUpdate, BlogPostDelete, ProfileBlog, UserProfile
+from project.models import BlogPost, BlogPostTags, UserProfile
+from project.views import (BlogPostCreate, HomePageView, BlogPostDetail, 
+	        BlogPostUpdate, BlogPostDelete, ProfileBlog, UserDashboard,
+	        BlogTags, AboutPageView, FollowUser, UnfollowUser)
+
+from selenium import webdriver
 
 
-class ViewTest(TestCase):
+class ProfilePageTest(LiveServerTestCase):
 
-	def SetUp(self):
-		self.client = Client()
+    fixtures = ['user_data.json', 'post_data.json',
+                'tag_data.json', 'user_profile.json']
+   
+    def setUp(self):
+    	self.driver = webdriver.Firefox()
+        self.client = Client()
 
-	def test_profile_page(self):
-		response = self.client.get('/eric/')
-		self.assertEquals(response.status_code, 200)
+    def test_profile_page(self):
+    	driver = self.driver
+        self.driver.get(
+            '{0}{1}'.format(self.live_server_url, '/eric/')
+        )
+        elem = driver.find_element_by_xpath('/html/body/div[2]/div/div/ul/h4/a')
+        self.assertIn('Follow', self.driver.page_source)
+
+    def tearDown(self):
+    	self.driver.quit()
