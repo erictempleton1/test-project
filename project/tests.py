2f3ce5cb0395e57d4a40ed7c079a50c4c9887b1f
erictempleton1 <erictempleton1@gmail.com>
2014-12-21 10:05:25 -0500

    finishing test for user profile follow

--- project/tests.py
+++ project/tests.py
@@ -17,13 +17,17 @@ class ProfilePageTest(LiveServerTestCase):
     	self.driver = webdriver.Firefox()
         self.client = Client()
 
-    def test_profile_page(self):
+    def test_follow(self):
+    	"""
+    	Tests follow button on user profile page.
+    	Un-auth'd user should be directed to login
+    	"""
     	driver = self.driver
         self.driver.get(
             '{0}{1}'.format(self.live_server_url, '/eric/')
         )
-        elem = driver.find_element_by_xpath('/html/body/div[2]/div/div/ul/h4/a')
-        self.assertIn('Follow', self.driver.page_source)
+        self.driver.find_element_by_xpath('/html/body/div[2]/div/div/ul/h4/a/button').click()
+        self.assertIn('Username', self.driver.page_source)
 
     def tearDown(self):
     	self.driver.quit()
