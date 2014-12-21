cc8af11ee4d6a20bae6dc5646b0e02737816e052
erictempleton1 <erictempleton1@gmail.com>
2014-12-21 10:29:15 -0500

    started logged in follow click tests

--- project/tests.py
+++ project/tests.py
@@ -20,14 +20,34 @@ class ProfilePageTest(LiveServerTestCase):
     def test_follow(self):
     	"""
     	Tests follow button on user profile page.
-    	Un-auth'd user should be directed to login
+    	User directed to login page if follow button is clicked,
+    	and they are not already logged in.
     	"""
     	driver = self.driver
         self.driver.get(
             '{0}{1}'.format(self.live_server_url, '/eric/')
-        )
-        self.driver.find_element_by_xpath('/html/body/div[2]/div/div/ul/h4/a/button').click()
+            )
+
+        # click follow button
+        self.driver.find_element_by_xpath(
+        	'/html/body/div[2]/div/div/ul/h4/a/button').click()
+
+        # check that un-authd user is taken to login page on follow click
         self.assertIn('Username', self.driver.page_source)
+
+    def test_follow_loggedin(self):
+    	driver = self.driver
+    	self.driver.get(
+    		'{0}{1}'.format(self.live_server_url, '/accounts/login/')
+    		)
+    	
+    	# log in as example user
+    	self.driver.find_element_by_id('id_username').send_keys('eric')
+    	self.driver.find_element_by_id('id_password').send_keys('eric')
+    	self.driver.find_element_by_xpath(
+    		'/html/body/div[2]/div/div/form/input[2]').click()
+
+        self.assertIn('Logout', self.driver.page_source)
 
     def tearDown(self):
     	self.driver.quit()
