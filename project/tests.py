9a84d3330aa29833839eb72c51efc788236f1fce
erictempleton1 <erictempleton1@gmail.com>
2014-12-22 06:26:43 -0500

    added tests for add/remove follow

--- project/tests.py
+++ project/tests.py
@@ -1,6 +1,8 @@
-from django.test import TestCase, LiveServerTestCase
-from django.test import TestCase, Client
 from django.conf import settings
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.test import TestCase, LiveServerTestCase
+from django.shortcuts import get_list_or_404, get_object_or_404
 from project.models import BlogPost, BlogPostTags, UserProfile
 from project.views import (BlogPostCreate, HomePageView, BlogPostDetail, 
 	        BlogPostUpdate, BlogPostDelete, ProfileBlog, UserDashboard,
@@ -56,7 +58,7 @@ class ProfilePageTest(LiveServerTestCase):
 
     def test_follow_self(self):
     	"""
-    	Test to be sure follow button is hidden for if user views own profile.
+    	Test to be sure follow button is hidden if user views own profile.
     	"""
     	driver = self.driver
     	self.login_example_user()
@@ -68,5 +70,52 @@ class ProfilePageTest(LiveServerTestCase):
         # 'pull-right' only exists when follow/unfollow button is shown
         self.assertNotIn('pull-right', self.driver.page_source)
 
+    def follow_exists(self):
+    	""" 
+    	Query to see if follow exists between example user 
+    	and another user.
+    	"""
+    	user_follow = get_object_or_404(User, username='bill')
+        eric = get_object_or_404(User, username='eric')
+        me, me_created = UserProfile.objects.get_or_create(user=eric)
+        follow_exists = me.following.filter(user=user_follow).exists()
+        return follow_exists
+
+
+    def test_follow_add(self):
+        """ Test click to be sure that 'Follow' adds user """
+    	driver = self.driver
+    	self.login_example_user()
+
+        # visit other user's profile page
+    	self.driver.get(
+    		'{0}{1}'.format(self.live_server_url, '/bill/')
+    		)
+
+        # once follow is clicked, exists() is True
+        self.driver.find_element_by_xpath(
+        	'/html/body/div[2]/div/div/ul/h4/a/button').click()
+
+        self.assertEqual(self.follow_exists(), True)
+
+    def test_follow_remove(self):
+    	driver = self.driver
+    	self.login_example_user()
+
+    	self.driver.get(
+    		'{0}{1}'.format(self.live_server_url, '/bill/')
+    		)
+
+        # click once to follow
+    	self.driver.find_element_by_xpath(
+    		'/html/body/div[2]/div/div/ul/h4/a/button').click()
+
+    	# click again to unfollow
+    	self.driver.find_element_by_xpath(
+    		'/html/body/div[2]/div/div/ul/h4/a/button').click()
+
+    	self.assertEqual(self.follow_exists(), False)
+
     def tearDown(self):
     	self.driver.quit()
+
