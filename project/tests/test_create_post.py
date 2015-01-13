6a12361def02126e354069a3ae3fd59c50080174
erictempleton1 <erictempleton1@gmail.com>
2015-01-12 21:48:33 -0500

    adding test for create post

---
+++ project/tests/test_create_post.py
@@ -0,0 +1,48 @@
+from django.conf import settings
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.test import TestCase, LiveServerTestCase
+from django.shortcuts import get_list_or_404, get_object_or_404
+from project.models import BlogPost, BlogPostTags, UserProfile
+from project.views import (BlogPostCreate, HomePageView, BlogPostDetail, 
+	        BlogPostUpdate, BlogPostDelete, ProfileBlog, UserDashboard,
+	        BlogTags, AboutPageView, FollowUser, UnfollowUser)
+
+from selenium import webdriver
+
+class CreatePostTest(LiveServerTestCase):
+
+    fixtures = ['user_data.json', 'post_data.json',
+	            'tag_data.json', 'user_profile.json']
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.client = Client()
+
+    def login_example_user(self):
+        """ Log in when needed """
+        driver = self.driver
+        self.driver.get(
+    		'{0}{1}'.format(self.live_server_url, '/accounts/login/'))
+
+    	self.driver.find_element_by_id('id_username').send_keys(
+    		settings.EXAMPLE_USERNAME)
+    	self.driver.find_element_by_id('id_password').send_keys(
+    		settings.EXAMPLE_PASSWORD)
+
+    	self.driver.find_element_by_xpath(
+    		'/html/body/div[2]/div/div/form/input[2]').click()
+
+    def test_page_load(self):
+    	"""
+    	Basic test to ensure create page loads for auth'd user
+    	"""
+    	driver = self.driver
+    	self.login_example_user()
+
+    	self.driver.get(
+    		'{0}{1}'.format(self.live_server_url, '/create/'))
+    	self.assertIn('Title', self.driver.page_source)
+
+    def tearDown(self):
+    	self.driver.quit()
