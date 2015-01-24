444746e1df98dba23d220ca7627e776993ece158
Eric Templeton <erictempleton1@gmail.com>
2015-01-24 02:52:14 +0000

    added funtion which random selects users to create posts for

--- project/test_data/data_create.py
+++ project/test_data/data_create.py
@@ -1,3 +1,4 @@
+import random
 from django.contrib.auth.models import User
 from project.models import BlogPost, BlogPostTags, UserProfile
 
@@ -39,5 +40,21 @@ def single_user_posts(username, num):
 		current_user.save()
 	return BlogPost.objects.all().order_by('-id')[:num-1]
 
+def random_user_posts(num):
+	all_users = User.objects.all()
+	usernames = [str(user.usernames) for user in all_users]
+	
+	for post in range(1,num):
+		current_user = User.objects.get(usernamer=random.choice(usernames)
+		current_user.blogpost_set.create(
+			title=fake.sentence(
+				nb_words=5,
+				variable_nb_words=True)[:-1],
+			content=fake.paragraph(
+				nb_sentences=10,
+				variable_nb_sentences=True)
+			)
+		current_user.save()
+	return BlogPost.objects.all().order_by('-id')[:num-1]
 
 
