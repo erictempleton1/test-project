cabc5da994d70cfb79e5b0c02c623b8c4bfdff6c
Eric Templeton <erictempleton1@gmail.com>
2015-01-26 06:32:12 -0500

    added models to admin

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from project.models import BlogPost, BlogPostTags, UserProfile
+
 
 # Register your models here.
+models = [BlogPost, BlogPostTags, UserProfile]
+admin.site.register(models)
