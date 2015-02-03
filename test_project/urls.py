a98f966c1997473bdc84725fa3c1ef6fb46272c4
Eric Templeton <erictempleton1@gmail.com>
2015-02-03 07:21:36 -0500

    added glyphicon link for search page

--- test_project/urls.py
+++ test_project/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'test_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls'))
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('project.urls', namespace="project")),
+    url(r'^', include('project.urls', namespace='project')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^summernote/', include('django_summernote.urls')),
     url(r'^home/search/', include('haystack.urls')),
